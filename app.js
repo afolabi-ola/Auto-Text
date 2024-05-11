2cf67ea4d1922e36f722b974febf4d989553230d
afolabi-ola <afolabiquadri28@gmail.com>
2024-05-11 12:05:15 +0100

    autotext commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const text = new Typed(".auto-text", {
+  strings: [
+    "Afolabi 😍.",
+    "a Programmer 🖥🖥💻💻,",
+    "a Frontend Developer 👌👌.",
+  ],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
